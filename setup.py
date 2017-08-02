3d179acadcf1d3ed82a7caba97b8266c15e798be
Mark Flanagan <mark.flanagan@gmail.com>
2017-08-01 20:55:56 -0400

    setting up for pypi upload

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name="wsgi-monitor3",
     version="1.0",
-    description="Auto-reload WSGI server when files change.",
-    url="http://github.org/flanaman/wsgi-monitor3",
-    author="MarkFlanagan",
+    description="Auto-reload WSGI server when files change - Python 3",
+    author="Mark Flanagan",
     authoremail="mark.mflanagan@gmail.com",
+    url="https://github.com/flanaman/wsgi_monitor3",
+    download_url="https://github.com/flanaman/wsgi_monitor3/archive/1.0.tar.gz",
     py_modules=[
         "wsgi_monitor3",
     ],
